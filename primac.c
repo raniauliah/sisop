93bd4ce9c757fd7b71f1b237416fc5b78ca9092b
ranitera <raniaulia72@gmail.com>
2015-11-02 18:46:04 +0700

    mencari bilangan prima menggunakan thread

--- primac.c
+++ primac.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
+#include<pthread.h>
+
+int count_p=0;
+
+void *count_prime(void *args){
+	int j, counter=0;
+	int *i=(int *)args;	
+
+	for(j=1; j<=*i; j++){
+		if(*i%j==0) counter++;
+	}
+	if(counter==2) count_p++;
+}
 
 int main()
 {
-	int angka, i, j, counter,count_p=0;
+	pthread_t thread1;
+	int angka, i, j, counter;
 	scanf("%d", &angka);
 	
 	for(i=2; i<angka; i++){
-		counter=0;
-		for(j=1; j<=i; j++){
-			if(i%j==0) counter++;
-		}
-		if(counter==2) count_p++;
+		pthread_create(&thread1, NULL, count_prime, (void *)&i);
+		pthread_join(thread1, NULL);
 	}
 	
 	printf("Jumlah bilangan prima: %d\n", count_p);
