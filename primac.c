363d866d0fff1198fe9f18a7b8948f09f370e589
Luqman Ahmad <luqman_ahmads@ymail.com>
2015-11-08 23:25:09 +0700

    update multithread

--- primac.c
+++ primac.c
@@ -5,14 +5,14 @@
 struct arg_struct {
 	int bil;
 	int* count_ptr;
-}
+};
 
 void *count_prime(void *args){
 	int j, counter=0;
 	struct arg_struct* argumen=(struct arg_struct*)args;	
 
-	for(j=1; j<=argumen->a; j++){
-		if(argumen->a%j==0) counter++;
+	for(j=1; j<=(argumen->bil); j++){
+		if((argumen->bil)%j==0) counter++;
 	}
 	if(counter==2) *(argumen->count_ptr)=*(argumen->count_ptr)+1;
 }
@@ -24,12 +24,12 @@ int main()
 	
 	scanf("%d", &angka);
 	
-	struct arg_struct args=(struct arg_struct*)malloc(sizeof(struct arg_struct)*(angka+1));
+	struct arg_struct* args=(struct arg_struct*)malloc(sizeof(struct arg_struct)*(angka+1));
 	pthread_t* thread=(pthread_t*)malloc(sizeof(pthread_t)*(angka+1));
 	
 	for(i=2; i<angka; i++){
 		args[i].bil=i;
-		args[i].counter_ptr=&count;
+		args[i].count_ptr=&count;
 		pthread_create(&thread[i], NULL, count_prime, &args[i]);
 	}
 	
